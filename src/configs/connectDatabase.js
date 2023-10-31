1544a5cc5c27a1f3e6a0eddda05cc930992421c1
Tiện Thanh <tienmt@imt-soft.com>
2023-10-31 15:59:10 +0700

    Update code

--- src/configs/connectDatabase.js
+++ src/configs/connectDatabase.js
@@ -1,16 +1,8 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize("dev-project-app", "root", "tien2000", {
+const sequelize = new Sequelize("db_dev_project", "root", "", {
   host: "localhost",
   dialect: "mysql",
 });
-
-// Trường hợp kết nối với máy chủ https://www.db4free.net/ ---------------------
-
-// const Sequelize = require("sequelize");
-// const sequelize = new Sequelize("t2kproject", "tien2000", "tien2000", {
-//   host: "db4free.net",
-//   dialect: "mysql",
-// });
 
 // ------------------------------------------------------------------------------
 
