f20f1b5b1aefd4c079de0bf3e0a036c9f9522b2a
Tiện Thanh <tienmt@imt-soft.com>
2023-10-31 16:08:00 +0700

    u

--- src/configs/connectDatabase.js
+++ src/configs/connectDatabase.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize("db_dev_project", "root", "", {
+const sequelize = new Sequelize("db_dev_project", "tien2000", "tien2000", {
   host: "localhost",
   dialect: "mysql",
 });
